10f19326e8b8f126ff0a88bd89e1c72384d259b6
amiranify <110248739+amiranify@users.noreply.github.com>
2023-03-04 23:03:36 +0300

    отредактировал код

--- src/main/java/springbootdemo/UserCrudApplication.java
+++ src/main/java/springbootdemo/UserCrudApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserCrudApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserCrudApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(UserCrudApplication.class, args);
+    }
 }
